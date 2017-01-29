086309f499be85d525e29d7448bdf38c3e6db8ac
Joanna Kolbe <joannakolbe@gmail.com>
2017-01-29 18:50:56 -0500

    next and previous product pages

--- src/containers/product/product.js
+++ src/containers/product/product.js
@@ -1,29 +1,46 @@
 import React, {Component} from 'react';
 import $ from 'jquery';
 
+import RaisedButton from 'material-ui/RaisedButton';
+import IconRight from 'material-ui/svg-icons/navigation/chevron-right';
+import IconLeft from 'material-ui/svg-icons/navigation/chevron-left';
+
 import ProductList from '../../components/product/product-list';
 
 export default class Product extends Component {
 
 	ACCESS_KEY = 'MDo2NWI0NTY4Yy1kZjEzLTExZTYtOTk3Zi1hN2E3NTQ5NzNlMzY6M0FZV1NWRWNOa1VsNU5jalJ2eURuUHBwbzBxWlNHOWxGalhC';
-	API_URL = 'https://lcboapi.com/products?access_key='+this.ACCESS_KEY;
+	BASE_URL = 'https://lcboapi.com';
+	API_URL = '/products?access_key='+this.ACCESS_KEY;
 
 	constructor() {
 		super();
 
 		this.state = {
-			products: []
-		};		
+			products: [],
+			is_first_page: true,
+			is_final_page: false,
+			next_page_path: "",
+			previous_page_path: ""
+		};	
+
+		this.getProducts = this.getProducts.bind(this);	
 	}
 
 	getProducts(overWriteURL) {
-		$.get(overWriteURL ? overWriteURL : this.API_URL).then(
+		$.get(overWriteURL ? this.BASE_URL+overWriteURL : this.BASE_URL+this.API_URL).then(
 			(res) => { this.setState({
-				products: res.result
-				})
-				console.log(res);
-			}
+				products: res.result,
+				is_first_page: res.pager.is_first_page,
+				is_final_page: res.pager.is_final_page,
+				next_page_path: res.pager.next_page_path,
+				previous_page_path: res.pager.previous_page_path,
+			})}
 		);
+	}
+
+	test(overWriteURL) {
+		console.log(overWriteURL);
 	}
 
 	componentDidMount() {
@@ -34,6 +51,26 @@ export default class Product extends Component {
 		return (
 			<div className="container">
 				<ProductList products={this.state.products} />
+
+				<div className="nav-buttons">
+					<RaisedButton
+						className="nav-buttons-prev"
+						onClick={()=>this.getProducts(this.state.previous_page_path)}
+						disabled={this.state.is_first_page}
+						label="Previous"
+						secondary={true}
+						icon={<IconLeft />} />
+
+					<RaisedButton
+						className="nav-buttons-next"
+						onClick={()=> this.getProducts(this.state.next_page_path)}
+						disabled={this.state.is_final_page}
+						label="Next"
+						labelPosition="before"
+						secondary={true}
+						icon={<IconRight />} />
+				</div>
+
 			</div>
 
 		)
